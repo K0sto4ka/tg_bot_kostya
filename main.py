b7d295953d70ae2e84c396f6562b97b040b42a55
echo bolt <nikbal044@gmail.com>
2022-10-22 12:50:57 +0300

    tg_bot

---
+++ main.py
@@ -0,0 +1,24 @@
+
+from telegram.ext import Updater, MessageHandler, Filters
+
+TOKEN = "5742426469:AAFd__uxzh5WWFnrD6gKRh2n00yFrXpTdZg"
+
+def echo(update, context):
+    txt = update.message.text
+    if txt.lower() in ['привет', 'салам']:
+        txt = "И тебе Салам мой друг!"
+    update.message.reply_text(txt)
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    print("Бот запущен")
+
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
