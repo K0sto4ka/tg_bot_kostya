fdc20680cb34746e7c0741b72d4cf46561fc4c3e
echo bolt <nikbal044@gmail.com>
2022-10-22 14:53:51 +0300

    comit 2

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CommandHandler
 TOKEN = "5742426469:AAFd__uxzh5WWFnrD6gKRh2n00yFrXpTdZg"
 
+from Wiki import search_wiki
+
 def echo(update, context):
     txt = update.message.text
     if txt.lower() in ['привет', 'салам']:
@@ -14,8 +16,24 @@ def echo(update, context):
 def start(update, context):
     update.message.reply_text("Это учебный бот.\nДля вывоза помощи наберите /help")
 
+
 def help(update, context):
     update.message.reply_text("Для вызова помощи наберите /help \nДля поиска в википедии наберите /wiki <текст для поиска>")
+
+
+def wikiword(update, context):
+    print(context.args)
+    word = " ".join(context.args)
+    if word:
+        update.message.reply_text("Идёт поиск...")
+        result, url = search_wiki(word)
+        update.message.reply_text(result+url)
+    else:
+        update.message.reply_text("Необходимо ввести текст для поиска")
+
+
+
+
 def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
@@ -23,9 +41,13 @@ def main():
 
 
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("wiki", wikiword))
+
+
+
+
     dp.add_handler(MessageHandler(Filters.text, echo))
-
-
     updater.start_polling()
     updater.idle()
 
